9019f6093691325b7c990ce3e8893f391dfffc75
Richard Darst <rkd@zgib.net>
2019-07-09 02:23:48 +0300

    Add tests of CSS in mode=both

--- sphinx_ext_substitution/test.py
+++ sphinx_ext_substitution/test.py
@@ -1,6 +1,6 @@
 import os
 from os.path import join as pjoin
-#os.environ['PYTHONPATH'] = os.getcwd()
+import re
 
 import pytest
 
@@ -14,7 +14,7 @@ def doc(build="_build-default", opts=""):
     return data
 
 @pytest.fixture(scope='session')
-def doc1():
+def doc1_default():
     """Default (replacement) version of document"""
     return doc()
 
@@ -30,35 +30,33 @@ def doc1_original():
 
 
 
-def test_role(doc1):
+def test_role(doc1_default):
+    assert "A0-original" in doc1_default
 
-    assert "A0-original" in doc1
+    assert "A1-id" not in doc1_default
+    assert "A1-original" in doc1_default
+    assert "A1-substitute" not in doc1_default
 
-    assert "A1-id" not in doc1
-    assert "A1-original" in doc1
-    assert "A1-substitute" not in doc1
+    assert "A2-id" not in doc1_default
+    assert "A2-original" not in doc1_default
+    assert "A2-substitute" in doc1_default
 
-    assert "A2-id" not in doc1
-    assert "A2-original" not in doc1
-    assert "A2-substitute" in doc1
+def test_role_inline_markup(doc1_default):
+    assert "A3-A3" not in doc1_default
+    assert "<em>A3-original</em>" in doc1_default
 
-def test_role_inline_markup(doc1):
-
-    assert "A3-A3" not in doc1
-    assert "<em>A3-original</em>" in doc1
-
-    assert "<em>A4-substitute</em>" in doc1
+    assert "<em>A4-substitute</em>" in doc1_default
 
 
-def test_directive(doc1):
-    assert 'A10-id' not in doc1
-    assert 'A10.1-original' in doc1
-    assert '<em>A10.2-original</em>' in doc1
+def test_directive(doc1_default):
+    assert 'A10-id' not in doc1_default
+    assert 'A10.1-original' in doc1_default
+    assert '<em>A10.2-original</em>' in doc1_default
 
-    assert 'A11.1-original' not in doc1
-    assert '<em>A11.2-original</em>' not in doc1
-    assert 'A11.1-substitute' in doc1
-    assert '<em>A11.2-substitute</em>' in doc1
+    assert 'A11.1-original' not in doc1_default
+    assert '<em>A11.2-original</em>' not in doc1_default
+    assert 'A11.1-substitute' in doc1_default
+    assert '<em>A11.2-substitute</em>' in doc1_default
 
 
 def test_mode_both(doc1_both):
@@ -76,6 +74,15 @@ def test_both_inline_markup(doc1_both):
     assert '<em>A10.2-original</em>' in doc1_both
     assert '<em>A11.2-original</em>' in doc1_both
     assert '<em>A11.2-substitute</em>' in doc1_both
+
+def test_both_css_roles(doc1_both):
+    assert re.search('class="substitute-original"[^>]*>\(NO_ID\)', doc1_both)
+    assert re.search('<strong class="substitute-original"[^>]*>\(A1-id\) A1-original', doc1_both)
+    assert re.search('class="substitute-original"[^>]*>\(A2-id\) A2-original', doc1_both)
+    assert re.search('class="substitute-replacement"[^>]*>A2-substitute', doc1_both)
+    # This test combines <em> in the original and CSS, which is probably not needed:
+    assert re.search('class="substitute-original"[^>]*>\(A3-id\) ?<em>A3-original', doc1_both)
+
 
 def test_mode_both(doc1_original):
     assert 'A2-original' in doc1_original
