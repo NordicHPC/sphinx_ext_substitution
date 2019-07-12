45445d426be6337cc21a609294682cd2d98d1ae7
Richard Darst <rkd@zgib.net>
2019-07-12 16:34:47 +0300

    Add get_substitutions to the util module

--- sphinx_ext_substitution/util.py
+++ sphinx_ext_substitution/util.py
@@ -1,4 +1,6 @@
 
+from .get_replacements import get_substitutions
+
 def make_sub_rst(id_, x):
     """Turn (id, original) into valid RST which can be inserted in a document for parsing."""
     return ":sub:`(%s)%s`"%(id_, x.replace("`", r"\`").strip())
